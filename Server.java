1996ae9acbb4b73bc21845fb8f6fa8c0a4bde422
Morgan <Morgan@localhost>
2010-11-23 17:37:43 -0500

    fixed concurrency stuff. Should be awesomer. Or I broke it, cause it worked even though it was broken in a way.

--- Server.java
+++ Server.java
@@ -185,9 +185,9 @@ public class Server extends AbstractServer {
     protected static class PriorityServerThread extends Thread{
         private volatile Integer readSemaphore = 0;
         private volatile Boolean writerIn = false;
-        private final Object writerLock = new Object();
+        private final Object writerInLock = new Object();
         private volatile Boolean readerIn = false;
-        private final Object readerLock = new Object();
+        private final Object readerInLock = new Object();
         private volatile Boolean isAlive = true;
         private boolean alreadySentMsg = false;
         
@@ -233,8 +233,8 @@ public class Server extends AbstractServer {
                          * the variable first.mode if OTHER or null are found as first.mode's value. 
                          */
                         if(first.getMode() != Message.Type.ATOMIC && first.getMode() != Message.Type.PULSE){
-                            synchronized(readerIn){
-                                synchronized(writerIn){
+                            synchronized(readerInLock){
+                                synchronized(writerInLock){
                                     if(readerIn || (!readerIn && !writerIn)){
                                         //TODO: Deal with locks and priorities...
                                         switch(first.getMode()){
@@ -288,8 +288,8 @@ public class Server extends AbstractServer {
                 out.println("Waiting for other threads to finish execution...");
                 alreadySentMsg = true;
             }
-            synchronized(readerIn){
-                synchronized(writerIn){
+            synchronized(readerInLock){
+                synchronized(writerInLock){
                     return (!readerIn && !writerIn);
                 }
             }
@@ -300,8 +300,8 @@ public class Server extends AbstractServer {
          * @return true if able to obtain read lock, false otherwise
          */
         public boolean getReadLock(){
-            synchronized(readerIn){
-                synchronized(writerIn){
+            synchronized(readerInLock){
+                synchronized(writerInLock){
                     if(writerIn)return false;
                     else{
                         readerIn = true;
@@ -319,8 +319,8 @@ public class Server extends AbstractServer {
          * @return true if able to obtain write lock, false otherwise
          */
         public boolean getWriteLock(){
-            synchronized(writerIn){
-                synchronized(readerIn){
+            synchronized(readerInLock){
+                synchronized(writerInLock){
                     //logger.log("WRITE LOCK OBTAINED? "+ !(readerIn || readSemaphore > 0 || writerIn));
                     return (writerIn = !(readerIn || readSemaphore > 0 || writerIn));
                 }
@@ -332,7 +332,7 @@ public class Server extends AbstractServer {
          * reader cleanup (releasing read lock for writer to come in).
          */
         public void releaseReadLock(){
-            synchronized(readerIn){
+            synchronized(readerInLock){
                 //logger.log("READ LOCK --"+"READERS: "+(readSemaphore-1));
                 if(--readSemaphore == 0) readerIn = false;
             }
@@ -342,7 +342,7 @@ public class Server extends AbstractServer {
          * clients to have access to data
          */
         public void releaseWriteLock(){
-            synchronized(writerIn){
+            synchronized(writerInLock){
                 writerIn = false;
                 //logger.log("WRITE LOCK RELEASED");
             }
